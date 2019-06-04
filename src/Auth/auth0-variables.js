7f9ca1799c7a93ebc067ed2bd1ec0abe11d32055
Curtis Timson <curt@live.co.uk>
2019-06-04 16:30:45 +0100

    Add netlify setup

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,17 @@
-export const AUTH_CONFIG = {
+const dev = {
   domain: 'ruletest.eu.auth0.com',
   clientID: '06ODevrf60QH8cYZ9ve6DkoboNIwcmBE',
   callbackUrl: 'http://localhost:3000/callback',
   audience: 'https://ruletest.com',
 }
+
+const prod = {
+  domain: 'ruletest.eu.auth0.com',
+  clientID: '06ODevrf60QH8cYZ9ve6DkoboNIwcmBE',
+  callbackUrl: 'http://auth0rulestest.netlify.com/callback',
+  audience: 'https://ruletest.com',
+}
+
+export const AUTH_CONFIG = process.env.REACT_APP_STAGE === 'production'
+  ? prod
+  : dev;
